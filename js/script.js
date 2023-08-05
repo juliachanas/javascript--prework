f83dac874a99f6c72e68b8a4463064b6ac5e3f02
Julia Chanas <juulia.chanas@gmail.com>
2023-08-05 15:49:38 +0200

    Add player move to js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// computer move
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -13,3 +14,22 @@ if (randomNumber == 1) {
 } 
 
 printMessage('Mój ruch to: ' + computerMove);
+
+// player move
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'kamień';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
